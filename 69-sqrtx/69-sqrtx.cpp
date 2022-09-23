fb5671b4c5389ad9dee578d06d978306d4761a87
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-09-23 15:03:15 +0530

    Time: 4 ms (57.83%), Space: 5.9 MB (25.43%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        //Binary search
+        long long start=0,end=INT_MAX,ans=0;
+        while(start<=end){
+            long long mid = start+(end-start)/2;
+            if(mid*mid<=x){
+                ans=mid;
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
+};
