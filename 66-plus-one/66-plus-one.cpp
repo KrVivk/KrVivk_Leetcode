bf4dadd4767870a02a794e43abe755b017a07da3
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-09-23 14:53:55 +0530

    Time: 5 ms (30.51%), Space: 8.6 MB (85.45%) - LeetHub

---
+++ 66-plus-one/66-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        for(int idx=n-1;idx>=0;--idx){
+            if(digits[idx]==9){
+                digits[idx]=0;
+            }
+            else{
+                digits[idx]++;
+                return digits;
+            }
+        }
+        digits.push_back(0);
+        digits[0]=1;
+        return digits;
+    }
+};
