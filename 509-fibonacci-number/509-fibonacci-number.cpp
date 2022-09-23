03ee1919581ea26c23f47b3a03ca8c82bb6dd169
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-09-23 15:24:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.47%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int fib(int n) 
+    {
+        if (n<=1)
+            return n;
+        int current = 0;
+        int prev1 = 1;
+        int prev2 = 0;
+        for (int i = 2; i <= n; i++) 
+        {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return current; 
+    }
+};
