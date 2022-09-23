9b5e535cfec391e4a379dfaa16fb55fbb8646958
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-09-23 15:12:40 +0530

    Time: 20 ms (26.26%), Space: 9.5 MB (88.11%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        long mask = (1L<<31),ans=0;
+        for(int i=31;i>=0;i--){
+            int count=0;
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]&mask)
+                    count++;
+            }
+            if(count%3!=0)
+                ans+=mask;
+            mask>>=1;
+        }
+        return ans;
+    }
+};
