ca0e67a08eb7e2e637f4720c4e7e7fa8f281d6b3
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-10-02 23:58:53 +0530

    Time: 7 ms (63.84%), Space: 6.8 MB (39.75%) - LeetHub

---
+++ 415-add-strings/415-add-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+    string result;
+    int i = num1.size()-1, j = num2.size()-1;
+    int carry = 0;
+    while(i >= 0 || j >= 0){
+        int sum = carry;
+        if(i >= 0){
+            sum += num1[i--] -'0'; 
+        }
+        if(j >= 0){
+            sum += num2[j--] - '0';
+        }
+        carry = sum > 9 ? 1 : 0;
+        result += to_string(sum%10);
+    }
+    if(carry) result += to_string(carry);
+    reverse(result.begin(), result.end());
+    return result;
+    }
+};
