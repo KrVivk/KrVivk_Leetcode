ec77b25cf90b4b2abeb9090f779b7c943afbb6fe
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-10-03 09:40:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (33.07%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.cpp
@@ -1,22 +1,11 @@
 class Solution {
 public:
     int sumBase(int n, int k) {
-        int ans=0;
-        while(n>=k){
-            int x=n%k;
-            ans*=10;
-            ans+=x;
-            n/=k;
-        }
-        if(n>0){
-            ans*=10;
-            ans+=n;
-        }
-        int total=0;
-        while(ans){
-            total+=(ans%10);
-            ans/=10;
-        }
-        return total;
+    int sum = 0;
+    while(n){
+        sum += n % k;
+        n /= k;
+    }
+    return sum;
     }
 };
