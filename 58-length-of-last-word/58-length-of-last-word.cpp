ae80e7540517e61c0643bace9d7935f3a62680c2
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-10-04 12:47:50 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (10.97%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) { 
+        reverse(s.begin(), s.end());
         int cnt = 0, flag = 0;
-        for(int i=s.length()-1; i>=0; i--)
+        for(int i=0; i<s.length(); i++)
         {
             if(s[i] != ' ')
             {
-                flag = 1; //this warns about the first occurance of the non-space char(i.e starting of the first word)
-                cnt++; //this counts the number of the chars in the word
+                flag = 1; 
+                cnt++; 
             }
-            else if(flag == 1 && s[i] == ' ') //this checks if that the current iteration is not the first space char(i.e some non-space chars are already before this because the flag is 1)
+            else if(flag == 1 && s[i] == ' ') 
             {
                 break;
             }
