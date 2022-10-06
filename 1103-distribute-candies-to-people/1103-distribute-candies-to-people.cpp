17aa433ac7ec4a616293a060bdd2b69464ce9e67
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-10-06 23:54:01 +0530

    Time: 6 ms (13.27%), Space: 6.5 MB (18.46%) - LeetHub

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    vector<int> distributeCandies(int candies, int num_people) {
-        int val=1,idx=0;
-        vector<int> ans(num_people,0);
-        while(candies){
-            ans[idx++]+=val;
-            candies-=val++;
-            if(idx==num_people) 
-                idx=0;
-            if(val>candies) 
-                val=candies;
+    vector<int> distributeCandies(int candies, int n) {
+         vector<int> res(n);
+        for (int i = 0; candies > 0; ++i) {
+            res[i % n] += min(candies, i + 1);
+            candies -= i + 1;
         }
-        return ans;
+        return res;
     }
 };
