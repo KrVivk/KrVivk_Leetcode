29ac2211eece124930790bf59dc2130f988734da
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-10-06 23:14:43 +0530

    Time: 11 ms (54.02%), Space: 10.2 MB (43.26%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int prod=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+                return 0;
+            if(nums[i]<0)
+                prod=-prod;
+        }
+       return prod;
+    }
+};
