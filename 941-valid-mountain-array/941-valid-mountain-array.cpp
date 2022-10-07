71300ede5b4261ac2947c10c0bd363d4d180a979
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-10-07 23:23:22 +0530

    Time: 65 ms (31.75%), Space: 22.5 MB (47.51%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
-    bool validMountainArray(vector<int>& arr) {
-        int n=arr.size();
-        int i =0;
-        while(i+1<n && arr[i]<arr[i+1])
-            i++;
-        if(i==0||i==n-1)
-            return false;
-        while(i+1<n && arr[i]>arr[i+1])
-            i++;
-        return i==n-1;
+    bool validMountainArray(vector<int>& A) {
+        int n = A.size(), i = 0, j = n - 1;
+        while (i + 1 < n && A[i] < A[i + 1]) i++;
+        while (j > 0 && A[j - 1] > A[j]) j--;
+        return i > 0 && i == j && j < n - 1;
     }
 };
