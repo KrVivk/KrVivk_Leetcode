f1f8f8720b0a476b354c6b6d3019c4c82d6fc6db
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-11-16 18:10:49 +0530

    Time: 6 ms (92.70%), Space: 8.5 MB (90.15%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void solve(vector<int>&nums, int idx, vector<vector<int>>&ans){
-    if(idx==nums.size()){
-        ans.push_back(nums);
-        return ;
+     void permute(vector<int>& nums,vector<vector<int>> &ans,int i){
+        if(i==nums.size()-1){
+            ans.push_back(nums);
+            return;
+        }
+        int temp[20] = {0};
+        for(int j=i;j<nums.size();j++){
+            if(temp[(nums[j])+10] == 0){
+                swap(nums[i],nums[j]);
+                permute(nums,ans,i+1);
+                swap(nums[i],nums[j]);
+            }
+            temp[nums[j]+10]++;
+        }
     }
-    for(int i=idx; i<nums.size();i++){
-        swap(nums[i],nums[idx]);
-        solve(nums,idx+1,ans);
-        swap(nums[i],nums[idx]);
-    }
-    return ;
-}
-    vector<vector<int>> permuteUnique(vector<int>& nums){
-    vector<vector<int>>ans;
-    solve(nums, 0, ans);
-    sort(begin(ans),end(ans));
-    ans.erase(unique(begin(ans), end(ans)),end(ans));
-    return ans;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        permute(nums,ans, 0);
+        return ans;
     }
 };
