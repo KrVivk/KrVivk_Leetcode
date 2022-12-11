0a249b9e2de7dd312d64327d8e5b670aa3043b03
KrVivk <114215552+KrVivk@users.noreply.github.com>
2022-12-12 00:04:07 +0530

    Added solution - LeetHub

--- Sorting Elements of an Array by Frequency - GFG/sorting-elements-of-an-array-by-frequency.cpp
+++ Sorting Elements of an Array by Frequency - GFG/sorting-elements-of-an-array-by-frequency.cpp
@@ -1,105 +1,50 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
- 
-
-bool mycomp(pair<int,int> a,pair<int,int> b)
-
-{
-
-    if(a.second>b.second)
-
-    {
-
-        return true;
-
+bool cmp(pair<int,int>p1,pair<int,int>p2){
+    if(p1.second!=p2.second){
+        return p1.second>p2.second;
     }
-
-    else if(a.second == b.second)
-
-    {
-
-        return a.first<b.first;
-
-    }
-
-    else{
-
-        return false;
-
-    }
-
+    return p1.first<p2.first;
 }
 
- 
+vector<int> customSort(vector<int>& arr){
+    int freq[60];
+    for(int i=0;i<60;i++){
+        freq[i]=0;
+    }
+    for(int i=0;i<arr.size();i++){
+        freq[arr[i]-1]++;
+    }
+    vector<pair<int,int>> vec;
+    for(int i=0;i<arr.size();i++){
+        vec.push_back({arr[i],freq[arr[i]-1]});
+    }
+    sort(vec.begin(),vec.end(),cmp);
+    vector<int> ans;
+    for(int i=0;i<vec.size();i++){
+        ans.push_back(vec[i].first);
+    }
+    return ans;
+}
 
 int main() {
-
- //code
-
- int t;
-
-    cin >> t;
-
-    for(int i=0;i<t;i++)
-
-    {
-
-        int n;
-
-        cin >> n;
-
-        vector <pair <int ,int>> ans;
-
-        map<int,int> m;
-
-        vector <int> arr;
-
-        for(int i=0;i<n;i++)
-
-        {
-
-            int x;
-
-            cin >> x;
-
-            arr.push_back(x);
-
-        }
-
-        for(int i=0;i<n;i++)
-
-        {
-
-            m[arr[i]]++;
-
-        }
-
-        for(auto i : m)
-
-        {
-
-            ans.push_back({i.first,i.second});
-
-        }
-
-        sort(ans.begin(),ans.end(),mycomp);
-
-        for(auto i: ans){
-
-            while(i.second--)
-
-            {
-
-                cout << i.first<<" ";
-
-            }
-
-        }
-
-        cout << endl;
-
-    }
-
+int t;
+cin>>t;
+while(t--){
+   int n;
+   cin>>n;
+   vector<int> arr;
+   for(int j=0;j<n;j++){
+       int x;
+       cin>>x;
+       arr.push_back(x);
+   }
+  vector<int> ans= customSort(arr);
+   for(int k=0;k<n;k++){
+       cout<<ans[k]<<" ";
+   }
+   cout<<endl;
+}
+return 0;
 }
