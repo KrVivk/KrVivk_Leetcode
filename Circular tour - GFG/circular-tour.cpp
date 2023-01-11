2a74fe9f3a699d732a043a411bf96863d3a1ae42
KrVivk <114215552+KrVivk@users.noreply.github.com>
2023-01-12 03:12:28 +0530

    Added solution - LeetHub

--- Circular tour - GFG/circular-tour.cpp
+++ Circular tour - GFG/circular-tour.cpp
@@ -24,36 +24,65 @@ class Solution{
   
     //Function to find starting point where the truck can start to get through
     //the complete circle without exhausting its petrol in between.
-    int tour(petrolPump p[],int n)
-    {
-        //considering first petrol pump as a starting point.
-        int start = 0;
-        int end=1;
-        int cur_pet = p[start].petrol-p[start].distance;
-        
-        //we run a loop while all petrol pumps are not visited and we have 
-        //reached first petrol pump again with 0 or more petrol.
-        while(end!=start or cur_pet <0)
-        {
-            //if current amount of petrol in truck becomes less than 0,
-            //then remove the starting petrol pump from tour.
-            while(cur_pet<0 and  start!=end)
-            {
-                //removing starting petrol pump and changing starting point. 
-                cur_pet -=p[start].petrol-p[start].distance;
-                start=(start+1)%n;
-                
-                //if 0 is being considered as start again, then there 
-                //is no possible solution.
-                if(start==0)
-                    return -1;
+    int tour(petrolPump p[], int n) {
+        //Your code here
+        int petrolHas = 0;
+        deque < int > q;
+        vector < bool > startingPoint(n, false);
+        int i = 0;
+        int ans = -1;
+        bool notFound = false;
+        bool Found = false;
+        while (true) {
+ 
+            if (q.empty() and startingPoint[i] == true) {
+                // to prevent from reconsidering a petrol pump which has been previously tested as a starting point for a complete
+                notFound = true;
+                break;
             }
-            //adding a petrol pump to current tour.
-            cur_pet +=p[end].petrol - p[end].distance;
-            end=(end+1)%n;
+            if (q.size() == n) {
+                // if it contains all n elements then it is a correct point for covering a circle
+                Found = true;
+                break;
+            }
+            // check if petrol of this petrol pump+ prevPetrol stored is enough for covering the distance to next petrol pump or not
+            int added = p[i].petrol + petrolHas;
+ 
+            if ((added - p[i].distance) < 0) {
+                // marking these petrol pumps as useless starting point
+                if (q.empty()) {
+                    startingPoint[i] = true;
+                }
+                while (!q.empty()) {
+                    // marking these petrol pump useless as they can never be starting point as the current petrol pump does not suffice conditions.
+                    int index = q.front();
+                    if (startingPoint[index] == false) {
+                        startingPoint[index] = true;
+                    }
+                    q.pop_front();
+                }
+                petrolHas = 0;
+            } else {
+                // adding a potiential starting point for covering the whole circle.
+                if (q.empty()) {
+                    q.push_back(i);
+                    startingPoint[i] = true;
+                    petrolHas += (p[i].petrol - p[i].distance);
+                } else {
+                    q.push_back(i);
+                    petrolHas += (p[i].petrol - p[i].distance);
+                }
+            }
+            // moving in a circular manner
+            i = (i + 1) % n;
         }
-        //returning starting point. 
-        return start;
+ 
+        if (notFound) {
+            return ans;
+        }
+ 
+        ans = q.front();
+        return ans;
     }
 };
 
