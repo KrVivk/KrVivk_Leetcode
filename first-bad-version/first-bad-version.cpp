90004bb3adb05adf08620b1599d49ef78c1e4bca
KrVivk <114215552+KrVivk@users.noreply.github.com>
2023-03-04 15:20:11 +0530

    Time: 0 ms, Memory: 5.8 MB - LeetHub

--- first-bad-version/first-bad-version.cpp
+++ first-bad-version/first-bad-version.cpp
@@ -4,19 +4,16 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        long int low = 1;
-        long int high = n;
-        long int mid;
-        long int result = n;
-        while(low<=high){
-            mid = (low+high)/2;
-            if(isBadVersion(mid)){
-                result=mid;
-                high =mid-1;
-            }
-            else
-                low = mid+1;
+    int left = 1;
+    int right = n;
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (isBadVersion(mid)) {
+            right = mid;
+        } else {
+            left = mid + 1;
         }
-        return result;
+    }
+    return left;
     }
 };
