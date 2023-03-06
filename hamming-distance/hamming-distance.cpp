446d0ceb560bfaea490f3c591fbddd932b16d182
KrVivk <114215552+KrVivk@users.noreply.github.com>
2023-03-06 23:05:24 +0530

    Time: 0 ms, Memory: 6 MB - LeetHub

---
+++ hamming-distance/hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        long mask =(1L<<31);
+        int count =0;
+        for(int i=31;i>=0;i--)
+        {
+            if((mask & x) != (mask&y))
+                count++;
+            mask>>=1;
+        }
+        return count;
+    }
+};
