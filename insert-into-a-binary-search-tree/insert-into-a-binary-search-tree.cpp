a2ea3b52d4e74301941378a3ec0633e009eaf923
KrVivk <114215552+KrVivk@users.noreply.github.com>
2023-03-11 04:22:29 +0530

    Time: 109 ms, Memory: 57 MB - LeetHub

--- insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -9,15 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+ /*
+ If the root is empty, the new tree node can be returned as the root node.
+
+Otherwise compare root. val is related to the size of the new value:
+
+If root->val is > than the target value, indicating that the target value should be inserted into the left subtree of the root.For thet we have to reach to the leftmost node of the left subtree.
+
+If root->val is < the target value, indicating that the new value should be inserted into the right subtree of the root.For thet we have to reach to the rightmost node of the right subtree.
+*/
+
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-    if(!root) 
-        return new TreeNode(val);
-    if(root->val > val) 
-        root->left = insertIntoBST(root->left,val);
-    else    
-        root->right = insertIntoBST(root->right,val);
-    return root;
+        if(!root) return new TreeNode(val);
+        TreeNode* temp=root;
+        while(true){
+            if(val<=temp->val){
+                if(temp->left!=NULL){
+                    temp=temp->left;
+                }
+                else{
+                    temp->left=new TreeNode(val);
+                    break;
+                }
+            }
+            else{
+                if(temp->right!=NULL){
+                    temp=temp->right;
+                }
+                else{
+                    temp->right=new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
     }
 };
